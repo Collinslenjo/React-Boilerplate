371d08d2d8aa7a45e80925d4366cac84166b47a5
collins lenjo <collinslenjo.cl@gmail.com>
2017-05-20 22:09:43 +0300

    Addition of server.js and server.babel.js debug

--- server.babel.js
+++ server.babel.js
@@ -2,6 +2,6 @@ import express from 'express';
 
 const app = express();
 
-app.use('/', express.static('public'));
+app.use('/', express.static('./src/client'));
 
 app.listen(process.env.PORT || 3000);
--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -7,6 +7,7 @@ class App extends React.Component {
   render () {
     return(
 	       <div>
+	       <h1>Hello Guys!</h1>
           <AboutComponent />
         </div>
     );
